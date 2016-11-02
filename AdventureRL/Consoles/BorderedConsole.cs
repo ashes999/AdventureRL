7f7ee6d8e122d27662a430d4860f475e887c24cd
ashes999 <ashes999@yahoo.com>
2016-11-02 12:49:35 -0400

    Hello World project with three consoles

---
+++ AdventureRL/Consoles/BorderedConsole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeenGames.AdventureRL.Consoles
+{
+    class BorderedConsole : SadConsole.Consoles.Console
+    {
+        public BorderedConsole(int width, int height, string title = "") : base(width, height)
+        {
+            SadConsole.Shapes.Box box = SadConsole.Shapes.Box.GetDefaultBox();
+            box.Width = width;
+            box.Height = height;
+            box.Foreground = Color.LightGray;
+
+            box.Draw(this);
+
+
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                this.Print((width - title.Length) / 2, 0, title);
+            }
+        }
+    }
+}
---
+++ AdventureRL/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using Console = SadConsole.Consoles.Console;
+using DeenGames.AdventureRL.Consoles;
+using Microsoft.Xna.Framework;
+
+namespace DeenGames.AdventureRL
+{
+    class Program
+    {
+        private const int StatusConsoleWidth = 24;
+        private const int MessageConsoleHeight = 6;
+        private const int GameWidth = 80;
+        private const int GameHeight = 25;
+
+        static void Main(string[] args)
+        {
+            // Setup the engine and creat the main window.
+            SadConsole.Engine.Initialize("IBM.font", GameWidth, GameHeight);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Engine.EngineStart += Engine_EngineStart;
+
+            // Hook the update event that happens each frame so we can trap keys and respond.
+            SadConsole.Engine.EngineUpdated += Engine_EngineUpdated;
+
+            // Start the game.
+            SadConsole.Engine.Run();
+        }
+
+        private static void Engine_EngineStart(object sender, EventArgs e)
+        {
+            // Clear the default console
+            SadConsole.Engine.ConsoleRenderStack.Clear();
+            SadConsole.Engine.ActiveConsole = null;
+
+            var viewConsole = new BorderedConsole(GameWidth - StatusConsoleWidth, GameHeight - MessageConsoleHeight);
+            var statusConsole = new BorderedConsole(StatusConsoleWidth, GameHeight - MessageConsoleHeight, "Status");
+            var messageConsole = new BorderedConsole(GameWidth, MessageConsoleHeight, "Messages");
+
+            viewConsole.Position = new Point(0, 0);
+            statusConsole.Position = new Point(viewConsole.Width, 0);
+            messageConsole.Position = new Point(0, viewConsole.Height);
+
+            SadConsole.Engine.ConsoleRenderStack.Add(viewConsole);
+            SadConsole.Engine.ConsoleRenderStack.Add(statusConsole);
+            SadConsole.Engine.ConsoleRenderStack.Add(messageConsole);
+        }
+
+        private static void Engine_EngineUpdated(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
