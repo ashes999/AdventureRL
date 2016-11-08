661f33bebd2d55939a8a85038911f4a32c15d8b7
ashes999 <ashes999@yahoo.com>
2016-11-08 12:47:58 -0500

    Default map; WASD movement

--- AdventureRL/Consoles/AreaViewConsole.cs
+++ AdventureRL/Consoles/AreaViewConsole.cs
@@ -4,6 +4,7 @@ using SadConsole.Game;
 using SadConsole;
 using SadConsole.Input;
 using DeenGames.AdventureRL.UI.SadConsoleMonogame.SadConsoleHelpers.Extensions;
+using Microsoft.Xna.Framework.Input;
 
 namespace DeenGames.AdventureRL.UI.SadConsoleMonogame.Consoles
 {
@@ -22,6 +23,8 @@ namespace DeenGames.AdventureRL.UI.SadConsoleMonogame.Consoles
             // Keyboard setup
             SadConsole.Engine.Keyboard.RepeatDelay = 0.07f;
             SadConsole.Engine.Keyboard.InitialRepeatDelay = 0.1f;
+
+            this.GenerateMap();
         }
 
         public override void Render()
@@ -38,20 +41,20 @@ namespace DeenGames.AdventureRL.UI.SadConsoleMonogame.Consoles
 
         public override bool ProcessKeyboard(KeyboardInfo info)
         {
-            if (info.KeysPressed.Contains(AsciiKey.Get(Microsoft.Xna.Framework.Input.Keys.Down)))
+            if (info.KeysPressed.Contains(AsciiKey.Get(Keys.Down)) || info.KeysPressed.Contains(AsciiKey.Get(Keys.S)))
             {
                 this.MovePlayerBy(new Point(0, 1));
             }
-            else if (info.KeysPressed.Contains(AsciiKey.Get(Microsoft.Xna.Framework.Input.Keys.Up)))
+            else if (info.KeysPressed.Contains(AsciiKey.Get(Keys.Up)) || info.KeysPressed.Contains(AsciiKey.Get(Keys.W)))
             {
                 this.MovePlayerBy(new Point(0, -1));
             }
 
-            if (info.KeysPressed.Contains(AsciiKey.Get(Microsoft.Xna.Framework.Input.Keys.Right)))
+            if (info.KeysPressed.Contains(AsciiKey.Get(Keys.Right)) || info.KeysPressed.Contains(AsciiKey.Get(Keys.D)))
             {
                 this.MovePlayerBy(new Point(1, 0));
             }
-            else if (info.KeysPressed.Contains(AsciiKey.Get(Microsoft.Xna.Framework.Input.Keys.Left)))
+            else if (info.KeysPressed.Contains(AsciiKey.Get(Keys.Left)) || info.KeysPressed.Contains(AsciiKey.Get(Keys.A)))
             {
                 this.MovePlayerBy(new Point(-1, 0));
             }
@@ -62,6 +65,26 @@ namespace DeenGames.AdventureRL.UI.SadConsoleMonogame.Consoles
         private void MovePlayerBy(Point point)
         {
             this.playerEntity.Position += point;
+        }
+
+        private void GenerateMap()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < this.Width; i++)
+            {
+                for (int j = 0; j < this.Height; j++)
+                {
+                    if (i == 0 || j == 0 || i == this.Width - 1 || j == this.Height - 1)
+                    {
+                        this[i + (j * this.Width)].GlyphIndex = '#';
+                    }
+                    else
+                    {
+                        this[i + (j * this.Width)].GlyphIndex = '.';
+                    }
+                }
+            }
         }
     }
 }
