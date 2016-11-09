6cb6a6899bc9304f52d656d5a40024707c37a903
ashes999 <ashes999@yahoo.com>
2016-11-09 18:14:41 -0500

    Refactor: extract Game class.

---
+++ AdventureRL/Game.cs
@@ -0,0 +1,36 @@
+﻿using DeenGames.AdventureRL.UI.SadConsoleMonogame.Screens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeenGames.AdventureRL.UI.SadConsoleMonogame
+{
+    class Game
+    {
+        public void Run()
+        {
+            // Setup the engine and creat the main window.
+            SadConsole.Engine.Initialize("IBM.font", Config.GameWidth, Config.GameHeight);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Engine.EngineStart += (sender, eventArgs) =>
+            {
+                // Clear the default console
+                SadConsole.Engine.ConsoleRenderStack.Clear();
+                SadConsole.Engine.ActiveConsole = null;
+
+                SadConsole.Engine.ConsoleRenderStack.Add(new AreaScreen());
+            };
+
+            // Hook the update event that happens each frame so we can trap keys and respond.
+            SadConsole.Engine.EngineUpdated += (sender, eventArgs) =>
+            {
+            };
+
+            // Start the game.
+            SadConsole.Engine.Run();
+        }
+    }
+}
--- AdventureRL/Program.cs
+++ AdventureRL/Program.cs
@@ -1,45 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Console = SadConsole.Consoles.Console;
-using Microsoft.Xna.Framework;
-using DeenGames.AdventureRL.UI.SadConsoleMonogame.Screens;
-
-namespace DeenGames.AdventureRL.UI.SadConsoleMonogame
+﻿namespace DeenGames.AdventureRL.UI.SadConsoleMonogame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Setup the engine and creat the main window.
-            SadConsole.Engine.Initialize("IBM.font", Config.GameWidth, Config.GameHeight);
-
-            // Hook the start event so we can add consoles to the system.
-            SadConsole.Engine.EngineStart += Engine_EngineStart;
-
-            // Hook the update event that happens each frame so we can trap keys and respond.
-            SadConsole.Engine.EngineUpdated += Engine_EngineUpdated;
-
-            // Start the game.
-            SadConsole.Engine.Run();
-        }
-
-        private static void Engine_EngineStart(object sender, EventArgs e)
-        {
-            // Clear the default console
-            SadConsole.Engine.ConsoleRenderStack.Clear();
-            SadConsole.Engine.ActiveConsole = null;
-
-            SadConsole.Engine.ConsoleRenderStack.Add(new AreaScreen());
-        }
-
-        private static void Engine_EngineUpdated(object sender, EventArgs e)
-        {
-
+            new Game().Run();
         }
     }
-
 }
