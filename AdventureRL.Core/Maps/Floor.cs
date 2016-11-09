8504957677f5f3c7043f85be768378c55a8a1296
ashes999 <ashes999@yahoo.com>
2016-11-09 13:38:46 -0500

    Mostly boiler-plate map gen Refactoring shortly

---
+++ AdventureRL.Core/Maps/Floor.cs
@@ -0,0 +1,19 @@
+﻿using SadConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace AdventureRL.Core.Maps
+{
+    // DERP: why do we have MonoGame here? We shouldn't.
+    public class Floor : CellAppearance
+    {
+        public Floor() : base(Color.DarkGray, Color.Transparent, '.')
+        {
+
+        }
+    }
+}
---
+++ AdventureRL.Core/Maps/Wall.cs
@@ -0,0 +1,19 @@
+﻿using SadConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace AdventureRL.Core.Maps
+{
+    // DERP: why do we have MonoGame here? We shouldn't.
+    public class Wall : CellAppearance
+    {
+        public Wall() : base(Color.White, Color.Transparent, '#')
+        {
+
+        }
+    }
+}
