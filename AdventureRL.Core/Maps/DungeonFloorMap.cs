b87fee334ba4fddd1e4694bfd05f9408632b1e09
ashes999 <ashes999@yahoo.com>
2016-11-09 14:33:32 -0500

    Separate map (model) from view.

---
+++ AdventureRL.Core/Maps/DungeonFloorMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeenGames.AdventureRL.Core.Maps
+{
+    public class DungeonFloorMap
+    {
+        private RogueSharp.Map tileData;
+
+        public DungeonFloorMap(int width, int height)
+        {
+            var mapCreationStrategy = new RogueSharp.MapCreation.RandomRoomsMapCreationStrategy<RogueSharp.Map>(width, height, 100, 15, 4);
+            this.tileData = RogueSharp.Map.Create(mapCreationStrategy);
+        }
+
+        public bool IsWalkable(int x, int y)
+        {
+            // TODO: make sure there are no entities there, eg. player, monster
+            return this.tileData.IsWalkable(x, y); 
+        }
+    }
+}
